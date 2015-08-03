e3e92851b747c6c0a0e8c95ac14f82def6f2e7c2
Konekoya <tank158r5155@yahoo.com.tw>
2015-08-04 07:41:55 +0800

    finishing the JavaScript part, working on CSS for the interface

--- js/main.js
+++ js/main.js
@@ -2,53 +2,47 @@
 
   var DateGenerator = function() {
     var doc = document;
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth();
-    var day = date.getDate();
-    var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    var year = '';
+    var month = '';
+    var day = '';
+    var firstDay = '';
+    var lastDay = '';
     var monthArr = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'];
     var weekDayArr = ['一', '二', '三', '四', '五', '六', '日'];
     var i = -1;
-    var len = lastDay.getDate() +1;
-    var count = firstDay.getDay() - 1;
+    var len = '';
+    var count = '';
     var span = '';
     var frag = doc.createDocumentFragment();
     var outPut = doc.querySelector('.output');
     var submitBtn = doc.querySelector('.submit-btn');
-    var yearPicker = '';
-    var monthPicker = '';
 
     submitBtn.addEventListener('click', function(e){
-      yearValue = Number(doc.querySelector('.year-picker').value || year);
-      monthPicker = doc.querySelector('.month-picker');
-      monthValue = Number(monthPicker.options[monthPicker.selectedIndex].value);
-      console.log(yearValue)
-      console.log(monthValue)
+      year = Number(doc.querySelector('.year-picker').value || year);
+      month = doc.querySelector('.month-picker');
+      month = Number(month.options[month.selectedIndex].value);
+      firstDay = new Date(year, month, 1);
+      lastDay = new Date(year, month + 1, 0);
+      len = lastDay.getDate() + 1;
+      count = firstDay.getDate() - 1;
 
-      firstDay = new Date(yearValue, monthValue, 1);
-      lastDay = new Date(yearValue, monthValue + 1, 0)
-      console.log(firstDay)
-      console.log(lastDay)
+      for (i = 1; i < len; i++) {
+          span = doc.createElement('span');
+          span.className = 'item';
+          if (count > 6) {
+            count = 0;
+          }
+          span.textContent = ( month + 1 ) + '/' + i + '(' + weekDayArr[count] + ')';
+          frag.appendChild(span);
+          count ++;
+      }
+
+      if (outPut) {
+        outPut.appendChild(frag);
+      }
+
       e.preventDefault();
     }, false);
-
-
-    for (i = 1; i < len; i++) {
-        span = doc.createElement('span');
-        span.className = 'item';
-        if (count > 6) {
-          count = 0;
-        }
-        span.textContent = ( month + 1 ) + '/' + i + '(' + weekDayArr[count] + ')';
-        frag.appendChild(span);
-        count ++;
-    }
-
-    if (outPut) {
-      outPut.appendChild(frag);
-    }
   };
 
   window.onload = function() {
