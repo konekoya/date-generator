7b8b64386976db6dbef17bcc7578d668e6156053
Konekoya <tank158r5155@yahoo.com.tw>
2015-08-14 07:31:51 +0800

    remove test JSON file and use array to get the chapter numbers

--- js/main.js
+++ js/main.js
@@ -21,9 +21,10 @@
     var monthPicker = doc.querySelector('.month-picker');
     var colors = ['2980B9', '2C3E50', '1695A3', '468966', 'B64926'];
     var randomColor = colors[Math.floor(Math.random() * colors.length)];
-    var chap = TEST;
-
-    console.log(randomColor)
+    var chap = [
+      ['joshua', 50],
+      ['kay', 20]
+    ];
 
     // set up default settings
     yearPicker.value = currentYear;
@@ -31,22 +32,26 @@
     doc.body.style.backgroundColor = '#' + randomColor;
 
     // console.log(chap)
-    console.log(chap.length )
-    for (var j = 0, leng = chap.length; j < leng; j++) {
-      console.log(chap[j])
+    for (var j = 0, lenj = chap.length; j < lenj; j++) {
+      // console.log(chap[j][0])
+      // console.log(chap[j][1])
+      for (var k = 0, lenk = chap[j].length; k < lenk; k++) {
+        console.log(chap[j][k])
+      }
+      // var single = chap[j][]
     }
 
     submitBtn.addEventListener('click', function(e) {
       year = Number(yearPicker.value || currentYear);
       yearPicker.value = year;
-      month = monthPicker;
-      month = Number(month.options[month.selectedIndex].value);
+      month = Number(monthPicker.options[monthPicker.selectedIndex].value);
       firstDay = new Date(year, month, 1);
       lastDay = new Date(year, month + 1, 0);
       len = lastDay.getDate() + 1;
       count = firstDay.getDay() - 1;
       div.innerHTML = '';
 
+      // create elements
       for (i = 1; i < len; i++) {
           span = doc.createElement('span');
           span.className = 'item';
